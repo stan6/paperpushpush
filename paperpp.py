92f979720484f181801258e2325a1c8c8592736a
stan6 <shinhwei@comp.nus.edu.sg>
2015-09-21 16:24:42 +0800

    Implemented functionality to find system configuration and added table for evaluation

--- paperpp.py
+++ paperpp.py
@@ -1,11 +1,41 @@
-import os, sys, getopt, platform,urllib.request
-
-from pylatex import Document, Section, Subsection, Table, Math, TikZ, Axis, \
-    Plot, Figure, Package
+from psutil import virtual_memory 
+import re, subprocess, multiprocessing, shutil, pwd, os, sys, getopt, platform,urllib.request, pylatex
+from pylatex import Document, Section, Subsection, Table, Math, TikZ, Axis, Plot, Figure, Package, Description, MultiColumn, MultiRow, Tabular 
 from pylatex.package import Package, Command
 from pylatex.numpy import Matrix
-from pylatex.utils import italic, escape_latex
+from pylatex.utils import italic, escape_latex, dumps_list
 
+
+defaultname = "our approach"
+currdir = os.getcwd()
+
+
+class SimpleDocument(Document):
+	def __init__(self,default_filepath):	
+		super().__init__(default_filepath=default_filepath,documentclass=None,data=None)
+
+	def dumps(self):
+		"""Represent the document as a string in LaTeX syntax.
+		Returns
+		-------
+		str
+		"""
+		sstr = super().dumps()
+		strarr = sstr.split('\n')[8:]
+		strarr = filter(lambda x: not "{document}" in x,strarr)
+		return '\n'.join(strarr)
+
+
+class Table(Figure):
+	def __init__(self, position):	
+		super().__init__(position=position)
+
+
+def current_user():
+  if pwd:
+    return pwd.getpwuid(os.geteuid()).pw_name
+  else:
+    return getpass.getuser()
 
 
 def download_doc_class(doclink):  
@@ -17,15 +47,112 @@ def download_doc_class(doclink):
 	return filename[0:filename.find('.')]   
 
 
+def add_paperprefix(doc):
+	doc.append(Command('begin', arguments='abstract'))
+	if defaultname == "our approach":
+		doc.append('We introduce a new approach that')
+	else:
+		doc.append('We introduce a new approach called \\toolname')
+	doc.append(Command('end', arguments='abstract'))
+	return doc
+
+
+def add_section(filename):
+	currfile = currdir+ os.sep +filename 
+	doc = SimpleDocument(currfile)	
+	return (doc,currfile)
+
+
+def get_processor_info():
+	if platform.system() == "Windows":
+		return platform.processor()
+	elif platform.system() == "Darwin":
+		os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+		command ="sysctl -n machdep.cpu.brand_string"
+		return subprocess.check_output(command).strip()
+	elif platform.system() == "Linux":
+		command = "cat /proc/cpuinfo"
+		all_info = subprocess.check_output(command, shell=True,universal_newlines=True).strip()
+		for line in all_info.split("\n"):
+			if "model name" in line:
+				return re.sub( ".*model name.*:", "", line,1)
+	return ""
+
+
+def cpu_countstr():
+	count = multiprocessing.cpu_count()
+	corestr = ""
+	if count == 1:
+		corestr = "single"
+	if count == 2:
+		corestr = "dual"
+	elif count == 4:
+		corestr = "quad"
+	elif count == 8:
+		corestr = "octa"
+	return corestr + "-core"
+
+
+def get_totmemory():
+	mem = virtual_memory()
+	mem_gib = mem.total/(1024.**3) 
+	return str('%.2f' % mem_gib)
+
+def machine_config():
+	machine_desc = "a " 
+	(numbit,rest) = platform.architecture()
+	machine_desc = str(numbit) + " machine with a"+ cpu_countstr() + " " + get_processor_info()+" processor and "+ get_totmemory() + "GB of memory."
+	return machine_desc
+
+def add_evaluation():
+	(doc, currfile) = add_section('evaluation')
+	doc.append('We perform an evaluation on TODO benchmark by comparing the effectiveness of our \\toolname. To evaluate the effectiveness of our approach, we aim to address the following research questions:')
+	with doc.create(Description()) as desc:
+		desc.add_item("RQ1", "What is the overall")
+		desc.add_item("RQ2", "How many")
+		desc.add_item("RQ3", "How many")
+	
+	doc.append(Subsection('Experimental Setup'))
+	doc.append("We evaluate \\toolname\\ on X subjects. Table~\\ref{substat} lists information about these subjects")
+
+	with doc.create(Table(position='!ht')) as wtable:	
+		table1 = Tabular('|c|c|c|c|')	
+		table1.add_hline()
+		table1.add_row(("Subject", "Description", "kLoc", "Tests"))
+		table1.add_hline()
+		table1.add_row(('x', 2, 1000, 4))
+		table1.add_hline()
+		table1.add_row(('y', 6, 1000, 8))
+		table1.add_hline()
+		wtable.append(table1)	
+		wtable.add_caption('Subject Program and Their Basic Statistics')
+		wtable.append('\\label{substat}')
+			#doc.append(table1)
+	doc.append("All experiments were performed on "+machine_config())
+	doc.generate_tex(currfile)
+
+
 def add_package(doc):
-	doc.packages.append(Package('geometry', options=['tmargin=1cm','lmargin=10cm']))
 	doc.packages.append(Package('algorithm2e',options=['ruled','linesnumbered']))
 	return doc
 
 def add_text(doc):
-	with doc.create(Section('Introduction')):
-		doc.append('Some regular text and some ' + italic('italic text. '))
-		doc.append(escape_latex('\nAlso some crazy characters: $&#{}'))
+	(idoc, introfile) = add_section("introduction")
+	idoc.append('Some regular text and some ')
+	idoc.generate_tex(introfile)	
+	
+	(bdoc, backgroundfile) = add_section("background")
+	bdoc.append('Some regular text and some ' + italic('italic text. '))
+	bdoc.generate_tex(backgroundfile)	
+	
+	add_evaluation()
+	doc.append(Section('Introduction'))
+	doc.append(Command('input', arguments='introduction.tex'))
+	doc.append(Section('Background'))
+	doc.append(Command('input', arguments='background.tex'))
+	doc.append(Section('Experiments'))
+	doc.append(Command('input', arguments='evaluation.tex'))
+
 	return doc
 
 
@@ -46,16 +173,24 @@ def main(argv):
 			outputfile = arg 
 	documentcls=download_doc_class(doclink)
 	print("Initialize docclass:",documentcls)
-	currdir = os.getcwd()
 	paperfile = currdir+ os.sep +"paper"
-	doc = Document(paperfile,documentcls,"","",False,None)
+	macrofile = currdir+ os.sep +"macro.tex" 
+	with open(macrofile,"w+") as f:
+		f.write("\\newcommand{\\toolname}{"+defaultname+"}")
+	
+	name = current_user()
+	print("Current_user:",name)
+	doc = Document(author=name,date='',title='Paper Title',maketitle=True, default_filepath=paperfile, documentclass=documentcls)
+	doc.preamble.append(Command('input', arguments='macro.tex'))
+
 	doc = add_package(doc)
-	doc.append(Command('title', arguments='Paper Title'))
+	doc = add_paperprefix(doc)
 	doc = add_text(doc)
 	doc.generate_tex(paperfile)
+	shutil.copy2(paperfile+".tex",paperfile+".tex.copy")	
 	doc.generate_pdf(paperfile)
-
-
+	shutil.copy2(paperfile+".tex.copy",paperfile+".tex")	
+	os.remove(paperfile+".tex.copy")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
